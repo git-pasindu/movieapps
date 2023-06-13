f91ea6347572a1ae1978f7cdb381cacb5109cbff
pasindu rangika <wapasindurangika@gmail.com>
2023-06-13 18:12:34 +0530

    movie api intergreted

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,20 @@ import SearchIcon from "./search.svg";
 import "./App.css";
 import MovieCard from "./MovieCard";
 
+const API_URL = import.meta.env.VITE_MOVIE_API_URL;
+
 function App() {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("superman");
+  });
+
   return (
     <>
       <div className="app">
