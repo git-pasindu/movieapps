f138f51898c9194062da035c939f1ceed4a28e60
pasindu rangika <wapasindurangika@gmail.com>
2023-06-13 16:24:28 +0530

    movie card styled

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function MovieCard() {
-  return <div>MovieCard</div>;
+  return (
+    <div className="movie">
+      <div>
+        <p>year</p>
+      </div>
+      <div>
+        <img src="https://via.placeholder.com/400" alt="title" />
+      </div>
+      <div>
+        <span>type</span>
+        <h3>title</h3>
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard;
